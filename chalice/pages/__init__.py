bb2c6c8407fbf5381008b573e4c0283e3a00abeb
Andri Mar <andri.m.jonsson@gmail.com>
2012-03-19 23:15:59 +0000

    Instruct the markdown module to output html5.

--- chalice/pages/__init__.py
+++ chalice/pages/__init__.py
@@ -8,6 +8,6 @@ pages = Blueprint('pages', __name__, template_folder = 'templates')
 def about():
     content = None
     with pages.open_resource('about.md') as f:
-        content = Markup(markdown.markdown(unicode(f.read(), 'UTF-8'), ['codehilite', 'html_tidy']))
+        content = Markup(markdown.markdown(unicode(f.read(), 'UTF-8'), extensions = ['codehilite', 'html_tidy'], output_format = 'html5'))
     return render_template('default_page.html', content = content, pagename = 'about me')
 
