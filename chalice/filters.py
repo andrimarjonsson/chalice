1c0c160ada31b4534933b61789e3e6985b8b529d
Andri Mar <andri.m.jonsson@gmail.com>
2012-06-25 22:28:54 +0200

    Updating time ago helper to use HTML5 time tags.

--- chalice/filters.py
+++ chalice/filters.py
@@ -7,8 +7,8 @@ def datetimeformat(datetime, timeago=True):
     readable = datetime.strftime('%Y-%m-%d @ %H:%M')
     if not timeago:
         return readable
-    iso_format = datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
-    return Markup('<span class=timeago title="%s">%s</span>' % (iso_format, readable))
+    iso_format = datetime.strftime('%Y-%m-%dT%H:%M:%S')
+    return Markup('<time class="timeago" datetime="%s">%s</time>' % (iso_format, readable))
 
 @app.template_filter()
 def markitup(text):
