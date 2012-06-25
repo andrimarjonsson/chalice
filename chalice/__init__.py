e9dc5f06dbe984b4dd8d384f0bc43b277ced67ab
Andri Mar <andri.m.jonsson@gmail.com>
2012-06-25 22:30:29 +0200

    Fixing the blog editor preview functionality. TODO: Need to find a better location for this "view".

--- chalice/__init__.py
+++ chalice/__init__.py
@@ -3,9 +3,11 @@ from chalice.pages import pages
 from chalice.auth import auth
 from chalice.upload import upload
 from chalice.extensions import db, app, lm
+from flask import Markup, request
 
 import chalice.filters
 import chalice.errors
+import chalice.helpers
 
 import default_config
 
@@ -14,7 +16,7 @@ __all__ = ['init_app']
 # -- TODO: Need to find a better spot for this.
 @app.route('/markdown_preview', methods=['POST'])
 def preview():
-    return Markup(markup(request.values['data']))
+    return Markup(helpers.markup(request.values['data']))
 
 def init_app():
     app.config.from_object(default_config)
