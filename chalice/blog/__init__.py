dbb8aa22d643d9b6b1dc714e410dfd31ac597277
Andri Mar <andri.m.jonsson@gmail.com>
2012-06-26 17:45:38 +0200

    Editing messages now supported.

--- chalice/blog/__init__.py
+++ chalice/blog/__init__.py
@@ -4,6 +4,8 @@ from chalice.blog.models import Post, Tag
 from chalice.helpers import generate_taglist
 from chalice.extensions import db
 
+from datetime import datetime
+
 blog = Blueprint('blog', __name__, template_folder = 'templates')
 
 @blog.route('/')
@@ -28,7 +30,7 @@ def show_posts_with_tag(tag):
     return render_template('taggedposts.html', posts = posts, tag = tag)
 
 # -- Admin section
-# -- TODO: Add error handling
+# -- TODO: Add better error handling
 
 @blog.route('/post/new', methods = ['GET', 'POST'])
 @login_required
@@ -41,6 +43,22 @@ def new_post():
         flash('Your post has been added.')
         return redirect(url_for('blog.show_posts'))
     return render_template('new_post.html')
+
+
+@blog.route('/post/edit/<int:post_id>', methods = ['GET', 'POST'])
+@login_required
+def edit_post(post_id):
+    post = Post.query.filter_by(id = post_id).first_or_404()
+    tags = ','.join([ tag.name for tag in post.tags])
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.text = request.form['text']
+        post.edited_at = datetime.now()
+        post.tags = generate_taglist(request.form['tags'])
+        db.session.commit()
+        flash('Your post has been edited.')
+        return redirect(url_for('blog.show_posts'))
+    return render_template('edit_post.html', post = post, tags = tags)
 
 @blog.route('/tag/new', methods = ['GET', 'POST'])
 @login_required
